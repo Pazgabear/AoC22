8dd8cc0f98e1a18da79e9a7c41bd9ef0dada99a0
Pazgabear <Pazgabear@users.noreply.github.com>
2022-12-05 10:15:03 +0100

    Day 5

---
+++ Day5/Helpers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    internal static class Helpers
+    {
+        public static string GetStacksDisplay(Stack<string>[] stacks)
+        {
+            var displayStacks = new Stack<string>[stacks.Length];
+            for (int i = 0; i < displayStacks.Length; ++i)
+                displayStacks[i] = new Stack<string>(stacks[i].Reverse());
+
+            var builder = new StringBuilder();
+            int maxStack = displayStacks.Max(s => s.Count);
+            int stackCount = displayStacks.Length;
+
+            for (int i = maxStack; i > 0; --i)
+            {
+                var boxes = new List<string>();
+                for (int j = 0; j < stackCount; ++j) 
+                {
+                    if (displayStacks[j].Count >= i)
+                        boxes.Add(displayStacks[j].Pop());
+                    else
+                        boxes.Add("   ");
+                }
+                builder.AppendLine(string.Join(' ', boxes));
+            }
+
+            builder.AppendLine($" {string.Join("   ", Enumerable.Range(1, stackCount))}");
+            return builder.ToString();
+        }
+    }
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,51 @@
+﻿using Day5;
+
+string[] input = File.ReadAllLines("input.txt");
+
+var initialState = input.TakeWhile(l => !string.IsNullOrWhiteSpace(l)).Reverse();
+int stackCount = int.Parse(initialState.First().Reverse().First(c => !char.IsWhiteSpace(c)).ToString());
+
+var stacks = new Stack<string>[stackCount];
+for (int i = 0; i < stackCount; ++i)
+    stacks[i] = new Stack<string>();
+
+foreach (string line in initialState.Skip(1))
+{
+    for (int i = 0; i < stackCount; ++i)
+    {
+        string box = line.Substring(i * 4, 3);
+        if (string.IsNullOrWhiteSpace(box))
+            continue;
+
+        stacks[i].Push(box.Trim());
+    }
+}
+
+Console.WriteLine(Helpers.GetStacksDisplay(stacks));
+
+var operations = input.SkipWhile(l => !string.IsNullOrWhiteSpace(l)).Skip(1);
+using var writer = new StreamWriter(File.OpenWrite("out.txt"));
+foreach (string op in operations)
+{
+    string[] t = op.Split(' ');
+    int count = int.Parse(t[1]);
+    int sourceStack = int.Parse(t[3]) - 1;
+    int destinationStack = int.Parse(t[5]) - 1;
+
+    /*
+    // Part 1
+    for (int i = 0; i < count; ++i)
+        stacks[destinationStack].Push(stacks[sourceStack].Pop());
+    */
+
+    // Part 2
+    var boxes = new List<string>();
+    for (int i = 0; i < count; ++i)
+        boxes.Add(stacks[sourceStack].Pop());
+
+    boxes.Reverse();
+    foreach (string box in boxes)
+        stacks[destinationStack].Push(box);
+}
+
+Console.WriteLine(Helpers.GetStacksDisplay(stacks));
