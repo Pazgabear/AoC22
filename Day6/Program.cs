3ed2c42951e2015d20854fd507fbc0bc3e4a2351
Pazgabear <Pazgabear@users.noreply.github.com>
2022-12-06 08:46:03 +0100

    Day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,25 @@
+﻿string input = File.ReadAllText("input.txt");
+
+// Part 1
+for (int i = 0; i < input.Length - 4; ++i)
+{
+    string chars = input.Substring(i, 4);
+
+    if (chars.Distinct().Count() == 4)
+    {
+        Console.WriteLine(i + 4);
+        break;
+    }
+}
+
+// Part 2
+for (int i = 0; i < input.Length - 14; ++i)
+{
+    string chars = input.Substring(i, 14);
+
+    if (chars.Distinct().Count() == 14)
+    {
+        Console.WriteLine(i + 14);
+        break;
+    }
+}
